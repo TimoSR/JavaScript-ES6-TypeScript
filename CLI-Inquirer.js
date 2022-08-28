9fca0f6030182442214ba38429df9e979bbae02f
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-08-28 16:39:02 +0200

    Update

---
+++ CLI-Inquirer.js
@@ -0,0 +1,22 @@
+// Require is a old way of using ESM modules
+// The standard is using native esm modules and importing them.
+
+const inquirer = require('inquirer');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your name?",
+  },
+  {
+    type: 'input',
+    name: 'numer',
+    message: "Give me a number!",
+  },
+];
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`Hi ${answers.name}!`);
+  console.log(`Your number is ${answers.numer}`)
+});
